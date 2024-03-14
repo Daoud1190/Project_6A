e343b8e0a862067e9ed8e5d91655dae5d3ea910c
Daoud1190 <146357859+Daoud1190@users.noreply.github.com>
2024-03-14 14:20:21 +0100

    Update MENU.ino

--- MENU.ino
+++ MENU.ino
@@ -8,6 +8,12 @@ const int optionX = 200;
 const int optionY[NUM_OPTIONS] = {200, 400, 600};
 const int optionWidth = 140;
 const int optionHeight = 60;
+const int backButtonX = 20;
+const int backButtonY = 20;
+const int backButtonWidth = 120;
+const int backButtonHeight = 40;
+
+bool optionSelected = false;
 
 void drawMenu() {
     canvas.fillCanvas(0);
@@ -19,13 +25,11 @@ void drawMenu() {
     canvas.pushCanvas(0, 0, UPDATE_MODE_DU4);
 }
 
-void setup() {
-    M5.begin();
-    M5.EPD.SetRotation(90);
-    M5.TP.SetRotation(90);
-    M5.EPD.Clear(true);
-    canvas.createCanvas(540, 960);
-    drawMenu();
+void drawBackButton() {
+    canvas.drawRect(backButtonX, backButtonY, backButtonWidth, backButtonHeight, 15);
+    canvas.setTextSize(2);
+    canvas.drawString("Retour", backButtonX + 10, backButtonY + 10);
+    canvas.pushCanvas(0, 0, UPDATE_MODE_DU4);
 }
 
 void performAction(const char *option) {
@@ -38,7 +42,16 @@ void performAction(const char *option) {
     } else if (strcmp(option, "Musique") == 0) {
         // Ajoutez ici le code pour l'action correspondant à "Musique"
     }
-    canvas.pushCanvas(0, 0, UPDATE_MODE_DU4);
+    drawBackButton();
+}
+
+void setup() {
+    M5.begin();
+    M5.EPD.SetRotation(90);
+    M5.TP.SetRotation(90);
+    M5.EPD.Clear(true);
+    canvas.createCanvas(540, 960);
+    drawMenu();
 }
 
 void loop() {
@@ -49,12 +62,22 @@ void loop() {
                 tp_finger_t FingerItem = M5.TP.readFinger(i);
                 int touchX = FingerItem.x;
                 int touchY = FingerItem.y;
-                for (int j = 0; j < NUM_OPTIONS; j++) {
-                    if (touchX >= optionX && touchX <= optionX + optionWidth &&
-                        touchY >= optionY[j] && touchY <= optionY[j] + optionHeight) {
-                        // Action correspondant à l'option j
-                        Serial.printf("Option sélectionnée: %s\n", options[j]);
-                        performAction(options[j]);
+                if (!optionSelected) {
+                    for (int j = 0; j < NUM_OPTIONS; j++) {
+                        if (touchX >= optionX && touchX <= optionX + optionWidth &&
+                            touchY >= optionY[j] && touchY <= optionY[j] + optionHeight) {
+                            // Action correspondant à l'option j
+                            Serial.printf("Option sélectionnée: %s\n", options[j]);
+                            performAction(options[j]);
+                            optionSelected = true;
+                        }
+                    }
+                } else {
+                    if (touchX >= backButtonX && touchX <= backButtonX + backButtonWidth &&
+                        touchY >= backButtonY && touchY <= backButtonY + backButtonHeight) {
+                        // Bouton "retour" pressé, réafficher le menu
+                        drawMenu();
+                        optionSelected = false;
                     }
                 }
             }
